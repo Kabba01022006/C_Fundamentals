8294aeb044f67e32e8cca85b2ba94fe44502095b
ShobhitKabba <shobhitkabba@gmail.com>
2026-02-27 23:10:03 +0530

    feat: add 2d arrays basic program

---
+++ 08_2dArrays/01_basics.c
@@ -0,0 +1,25 @@
+// int arr[3][5] = {{1,2,3,4,5},
+//                 {6,7,8,9,10},
+//                 {11,12,13,14,15}};
+
+// datatype arrayname[rows][columns]
+
+#include<Stdio.h>
+int main(){
+    int arr[2][2];
+    arr[0][0]=1;
+    arr[0][1]=2;
+    arr[1][0]=3;
+    arr[1][1]=4;
+    // 1 2
+    // 3 4
+
+    int firstArr[2][3] = {{1,2,3},{4,5,6}};
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",firstArr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
