5b8adc2c33c6a786112f8842a1a24daa57881259
shobhitkabba <shobhitkabba@gmail.com>
2026-04-06 18:33:43 +0530

    feat: add another method of printing a table in loops folder

--- 03_loops/07_7Problem4.c
+++ 03_loops/07_7Problem4.c
@@ -13,15 +13,15 @@
 //     return 0;
 // }
 
-#include<stdio.h>
-int main()
-{   
-    for(int i=19;i<=190;i=i+19)
-    {
-        printf("%d\n",i);
-    }
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {   
+//     for(int i=19;i<=190;i=i+19)
+//     {
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
 
 
 // #include<stdio.h>
@@ -35,3 +35,11 @@ int main()
 //     }
 //     return 0;
 // }
+
+#include<stdio.h>
+int main(){
+int a = 19;
+for(int i=1;i<=10;i++){
+    printf("%d\n",i*a);
+}
+}     //in this code we can change the value of a according to the table that we need 
